865b3ccd8b376d174b745dff4655417a1269e47b
Faheem835 <oyit8t@gmail.com>
2025-10-25 22:32:23 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "ANAYAT-AI~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUo5ZkdjQTY4OFZNVnFLR29PTWZhc2J4c2llc0lkakQycHpZbWlUV2pIOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSnlMWXdMMGJXeDN3ODVMdHFWdmlEaTNvdzlHVWdST2ZaNTd1eHdnRzVVVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNQ0pZdVdsUkhYZmFmSkJDanBiRUhPRGpmeWxZUExjTlN3MHVpZFBsTzNzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLc2dnSjB4dWM0TTlLUHpSVXNqa3NvcEt4am9UODJRdkVDZ0RVdlI3UjJ3PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFQN0xhczBBOGQzaW1qVm5iOGFkVUlrQy91QytYZmpqejAxNGVuY1lYVWM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldXeHp3RlpXKzBjc0c5dWpPYUR3VFMwSzJSem12N1V5QklGaXRLWWtIMVE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0M5SGFpT2ZwVGFiUnBvalVSZGE0dmdWaW9OaVhnRC93K3lmZmJBdFNubz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibURLZUtqSVFuUXN1bjJDMnRGN1Z0SlhzS2k2U1RSU1dhRDRsQm9xbjlqdz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImtiMjdxVTZ1SXZCOGJWZy9QcUJDNlQ5RVJ1aGJEZG5DaTVaa1ZLaEVsaGlULytHMzRlTXkyUFAvdnJkUHRaanBpaXdpQ0M5Nld0RlJ3RStOTEVpZkR3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjQyLCJhZHZTZWNyZXRLZXkiOiJzc0QyV2RJWGQyL01JU2pQMmR5SURJY0s0S1pnc0hCNlJSOGhrL1NDeHo4PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjkyMzM0MDkyODcxMEBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiJBQ0QzNzUwNjZDNzRGQ0Q0NTNGMDBBOEEyMUFFNTZFQyJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzYxMTIxMDg0fSx7ImtleSI6eyJyZW1vdGVKaWQiOiI5MjMzNDA5Mjg3MTBAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiQUM1QzVDMzNCMDg3OEQ4OUFEQjVBQjBDNzJCQjhDRTMifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc2MTEyMTA4NH1dLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiWjdFUUcxREIiLCJtZSI6eyJpZCI6IjkyMzM0MDkyODcxMDo2QHMud2hhdHNhcHAubmV0IiwibGlkIjoiOTYzNzUzNzUxNTEyNjk6NkBsaWQiLCJuYW1lIjoiRiJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDS3FRbWMwQ0VLeW00c2NHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiZE5ZYnRsQTdlQlB6U1Q3c1B5NzNFbHppSmgwK3RiR2thK2xMZlkxeEl4Zz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiR2tGKzVpMXdDSmJLQWVOenI1cDBLbTZzS3F2WFBEQStGRDFCSkZERDBBbHFCVHVaVjFxYWZ1VVNzRDFZU0M3MzZ0SDZkNnhvYXhFbnJYcWVaRlJOREE9PSIsImRldmljZVNpZ25hdHVyZSI6IlpRMEIxeE9wcHFDcEdLTGdLd3dLL0h4V3VXWkFHK29mWlhXdFBGblJEVlZYWjhrTkRpYjNzTlJmODVaNGhMcG1RMjVURzQxYmp0eTZ0VUhMMCtkbURBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTIzMzQwOTI4NzEwOjZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWFRXRzdaUU8zZ1Q4MGsrN0Q4dTl4SmM0aVlkUHJXeHBHdnBTMzJOY1NNWSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FVSUVnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzYxMTIxMDgxLCJsYXN0UHJvcEhhc2giOiJQV2s1QiIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBQWQzIn0=",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY FAIZAN-AI 🤍*",
+// set the auto reply massage on status reply  
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+// set true false for anti delete     
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat     
+WELCOME: process.env.WELCOME || "true",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/9mdHyGDL/shaban-md.jpg",
+// add custom menu and mention reply image url
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "FAIZAN-AI",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || " 『⸙聞⤹³⎯꯭̽𓆩ɪ͜τ,͡᥉‿ꄘɑ͢卄e͓̽e͓̽爪ᬄ𓆪聞⸙』",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923154937411",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "*: 𒁍⃝ꄘɑ᪳͢卄ɛ̽͜ɛ̽͜爪↰᚛:⃕‹✯›*",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ғᴀʜᴇʀᴍ ❣️*",
+// add bot owner name    
+ALIVE_VID: process.env.ALIVE_VID || "https://cdn-bandaheali.vercel.app/file/ADEEL-MD41321.jpg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ⚡",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+// make anti link true,false for groups 
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923337862496",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
